65f02b059d65120757e83680d329aace62f2d70e
TanushaVeraksa <54898478+TanushaVeraksa@users.noreply.github.com>
2022-11-29 00:22:42 +0300

    feat: create getMovingSum task

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -238,8 +238,12 @@ function toArrayOfSquares(arr) {
  *   [ 0, 0, 0, 0, 0]         => [ 0, 0, 0, 0, 0]
  *   [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] => [ 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 ]
  */
-function getMovingSum(/* arr */) {
-  throw new Error('Not implemented');
+function getMovingSum(arr) {
+  return arr.map((elem, index) => arr.slice(0, index).reduce((curr, acc) => {
+    // eslint-disable-next-line no-param-reassign
+    curr += acc;
+    return curr;
+  }, elem));
 }
 
 /**
