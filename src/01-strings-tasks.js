c57588eed2fdf8fad9f35946669af97eee2de2db
TanushaVeraksa <54898478+TanushaVeraksa@users.noreply.github.com>
2022-11-28 20:46:25 +0300

    feat: create extractNameFromTemplate task

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -65,8 +65,9 @@ function getStringFromTemplate(firstName, lastName) {
  *   'Hello, John Doe!' => 'John Doe'
  *   'Hello, Chuck Norris!' => 'Chuck Norris'
  */
-function extractNameFromTemplate(/* value */) {
-  throw new Error('Not implemented');
+function extractNameFromTemplate(value) {
+  const res = value.split(', ').slice(1).join('').split('!');
+  return res.join('');
 }
 
 
